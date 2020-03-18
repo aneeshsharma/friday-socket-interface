bd6ce0500c02d400347b2251b7d92709d34a8229
Anish Sharma <aneeshsharma15@outlook.com>
2020-03-18 21:46:52 +0530

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,7 @@ from setuptools import setup, find_packages
 setup(
 	name="Friday Socket Interface",
 	version="0.1",
+	author="Anish Sharma",
+	url="https://github.com/aneeshsharma/friday-socket-interface",
 	packages=find_packages(),
 )
